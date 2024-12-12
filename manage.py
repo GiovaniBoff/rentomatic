43bcc2011c4884b0f9dc29f0de6b3125c1d1a12e
Giovani Boff <giovanisouza15@gmail.com>
2024-12-11 22:42:06 -0300

    feat: add config to production ready

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 import os
 import json
+import signal
 import subprocess
 import time
 
@@ -10,6 +11,8 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 # Ensure an environment variable exists and has a value
 def setenv(variable, default):
     os.environ[variable] = os.getenv(variable, default)
+
+setenv("APPLICATION_CONFIG", "production")
 
 APPLICATION_CONFIG_PATH = "config"
 DOCKER_PATH = "docker"
@@ -32,10 +35,10 @@ def read_json_configuration(config):
 
 def configure_app(config):
     configuration =  read_json_configuration(config)
-    
+
     for key,value in configuration.items():
         setenv(key,value)
-        
+
 @click.group()
 def cli():
     pass
@@ -43,12 +46,12 @@ def cli():
 def docker_compose_cmdline(commands_string=None):
     config = os.getenv("APPLICATION_CONFIG")
     configure_app(config)
-    
+
     compose_file = docker_compose_file(config)
-    
+
     if not os.path.isfile(compose_file):
         raise ValueError(f"The file {compose_file} does not exist")
-    
+
     command_line = [
         "docker-compose",
         "-p",
@@ -56,10 +59,10 @@ def docker_compose_cmdline(commands_string=None):
         "-f",
         compose_file
     ]
-    
+
     if commands_string:
         command_line.extend(commands_string.split(" "))
-        
+
     return command_line
 
 def run_sql(statements):
@@ -70,21 +73,21 @@ def run_sql(statements):
         host=os.getenv("POSTGRES_HOSTNAME"),
         port=os.getenv("POSTGRES_PORT"),
     )
-    
+
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cursor = conn.cursor()
     for statement in statements:
         cursor.execute(statement)
-        
+
     cursor.close()
     conn.close()
-    
+
 def wait_for_logs(cmdline, message):
     logs = subprocess.check_output(cmdline)
     while message not in logs.decode("utf-8"):
         time.sleep(1)
         logs = subprocess.check_output(cmdline)
-        
+
 @cli.command()
 @click.argument("args", nargs=-1)
 def test(args):
@@ -111,6 +114,58 @@ def test(args):
     cmdline = docker_compose_cmdline("down")
     subprocess.call(cmdline)
 
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("subcommand", nargs=-1, type=click.Path())
+def compose(subcommand):
+    configure_app(os.getenv("APPLICATION_CONFIG"))
+    cmdline = docker_compose_cmdline() + list(subcommand)
+
+    try:
+        p = subprocess.Popen(cmdline)
+        p.wait()
+    except KeyboardInterrupt:
+        p.send_signal(signal.SIGINT)
+        p.wait()
+
+@cli.command()
+
+def init_postgres():
+    configure_app(os.getenv("APPLICATION_CONFIG"))
+
+    try:
+        run_sql([f"CREATE DATABASE {os.getenv('APPLICATION_DB')}"])
+    except psycopg2.errors.DuplicateDatabase:
+        print(
+            (
+                f"The database {os.getenv('APPLICATION_DB')} already",
+                "exists and will not be recreated",
+            )
+        )
+@cli.command()
+@click.argument("message", nargs=-1)
+def create_migration(message):
+    """
+    Create a new migration file that will automate a schema change.
+
+    :param message: The message for the revision history.
+    :return: None
+    """
+    env_vars = {
+        "POSTGRES_USER": "postgres",
+        "POSTGRES_PASSWORD": "postgres",
+        "POSTGRES_HOSTNAME": "localhost",
+        "APPLICATION_DB": "application",
+    }
+    for key, value in env_vars.items():
+        os.environ[key] = value
+    cmdline = [
+        "alembic",
+        "revision",
+        "--autogenerate",
+        "-m",
+        " ".join(message or ["Initial"]),
+    ]
+    subprocess.call(cmdline)
 
 if __name__ == "__main__":
     cli()
